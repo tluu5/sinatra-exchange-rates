11c0c5d8206b2ebfb7d610480ed698cc2ad5aa1d
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-31 02:53:03 +0000

    commit edited hash

--- app.rb
+++ app.rb
@@ -4,10 +4,13 @@ require "http"
 require "json"
 
 get("/") do
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
   raw_data = HTTP.get(api_url)
   raw_data_string = raw_data.to_s
-  @parsed_data = JSON.parse(raw_data_string)
-  
+  parsed_data = JSON.parse(raw_data_string)
+  currencies_hash = parsed_data.fetch("currencies")
+  currencies_hash.each do |code, name|
+  end
+
   erb(:homepage)
 end
