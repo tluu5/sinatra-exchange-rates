cea707bdb270780dfa4b51128b59f5492f061516
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-31 04:30:03 +0000

    commit working from currency to

--- app.rb
+++ app.rb
@@ -8,9 +8,16 @@ get("/") do
   raw_data = HTTP.get(api_url)
   raw_data_string = raw_data.to_s
   parsed_data = JSON.parse(raw_data_string)
-  currencies_hash = parsed_data.fetch("currencies")
-  currencies_hash.each do |code, name|
-  end
-
+  @currencies_hash = parsed_data.fetch("currencies")
   erb(:homepage)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @currencies_hash = parsed_data.fetch("currencies")
+  erb(:from_currency_to)
 end
