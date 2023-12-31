9a3a4effb1bb8ab652552abe17edd9581a320e6a
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-31 05:06:07 +0000

    commit working web

--- app.rb
+++ app.rb
@@ -21,3 +21,13 @@ get("/:from_currency") do
   @currencies_hash = parsed_data.fetch("currencies")
   erb(:from_currency_to)
 end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+  api_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  raw_data = HTTP.get(api_url)
+  parsed_data = JSON.parse(raw_data)
+  @result_hash = parsed_data.fetch("result")
+  erb(:to_currency)
+end
